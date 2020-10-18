0724452141b1765c2b1796336495afbc5cf198ec
kennetnnah <44839686+techagentng@users.noreply.github.com>
2020-10-18 09:58:52 +0100

    Updates

---
+++ precache-manifest.be014ed508b4da92e32c5d2c07ff78e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7a187ecce5907e6ac8afad4d8530f95",
+    "url": "/react-app-user-registeration/index.html"
+  },
+  {
+    "revision": "de7cbf34647834135052",
+    "url": "/react-app-user-registeration/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8910441b64d8599fab94",
+    "url": "/react-app-user-registeration/static/js/2.996a4b90.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-app-user-registeration/static/js/2.996a4b90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de7cbf34647834135052",
+    "url": "/react-app-user-registeration/static/js/main.8fffae6c.chunk.js"
+  },
+  {
+    "revision": "e1dae2c5312d89229e34",
+    "url": "/react-app-user-registeration/static/js/runtime-main.171f45e2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app-user-registeration/static/media/logo.5d5d9eef.svg"
+  }
+]);
