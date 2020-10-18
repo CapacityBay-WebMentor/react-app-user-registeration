e2e236f050fc67ca039b6ebf23a4284da42bb5c8
kennetnnah <44839686+techagentng@users.noreply.github.com>
2020-10-18 10:02:39 +0100

    welcome commit message

---
+++ src/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Nav(){
+  return(
+    <div>
+      <h1>
+        Hello world
+      </h1>
+    </div>
+  );
+}
+
+export default Nav;
--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Nav from './Nav';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Nav />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
